beed13c12d2a0e10cc52ddfef5b6edd3949d2f94
ffarall <37149322+ffarall@users.noreply.github.com>
2019-11-21 11:10:35 -0300

    Adding bouncing on sides of paddles

--- Pong/logic/game_logic.v
+++ Pong/logic/game_logic.v
@@ -75,6 +75,18 @@ module GameLogic(
         end else if ((ball_size_x + ball_pos_x) >= paddle_2_pos_x && (ball_pos_y + ball_size_y) >= paddle_2_pos_y && ball_pos_y <= (paddle_2_pos_y + paddle_2_size_y)) begin
             bounce = 1;
 
+        end else if ((ball_pos_y + ball_size_y) >= paddle_1_pos_y && ball_pos_x <= paddle_1_pos_y) begin
+            bounce = 2;
+
+        end else if ((ball_pos_y + ball_size_y) >= paddle_2_pos_y && (ball_pos_x + ball_size_x) >= (paddle_2_pos_y + paddle_2_size_x)) begin
+            bounce = 2;
+
+        end else if (ball_pos_y <= (paddle_1_pos_y + paddle_1_size_y) && ball_pos_x <= paddle_1_pos_y) begin
+            bounce = 2;
+
+        end else if (ball_pos_y <= (paddle_2_pos_y + paddle_2_size_y) && (ball_pos_x + ball_size_x) >= (paddle_2_pos_y + paddle_2_size_x)) begin
+            bounce = 2;
+
         end else begin
             bounce = 0;
         end
